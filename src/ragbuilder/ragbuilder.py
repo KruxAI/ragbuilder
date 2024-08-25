0d860aa486a410a4b1589a30d014a156c38a1735
“ashwinaravind” <ashwin@krux.ai>
2024-08-25 12:26:24 +0530

    Revert "counter.py"
    This reverts commit 82a63548b87e98acca470c41ab8b1e4ffd9e67d5.

--- src/ragbuilder/ragbuilder.py
+++ src/ragbuilder/ragbuilder.py
@@ -378,7 +378,7 @@ def parse_config(config: dict, db: sqlite3.Connection):
     enable_analytics = os.getenv('ENABLE_ANALYTICS', 'True').lower() == 'true'
     logger.info(f"enable_analytics = {enable_analytics}")
     if enable_analytics:
-        track_event()
+        track_event(config)
     logger.info(f"Initiating parsing config: {config}")
     disabled_opts=_get_disabled_opts(config)
     logger.info(f"Disabled options: {disabled_opts}")
