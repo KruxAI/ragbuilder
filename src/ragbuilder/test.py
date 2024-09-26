0a703d71a74bc0c9f0153b2f76090415063d05c9
“ashwinaravind” <ashwin@krux.ai>
2024-09-26 19:03:15 +0530

    Delete test.py

--- src/ragbuilder/test.py
+++
@@ -1,7 +0,0 @@
-from ragbuilder.data_processor import DataProcessor, DATA_PROCESSORS
-# Example usage
-data_source = "This is an <b>EXAMPLE</b> string, with punctuation and stopwords!"
-processor = DataProcessor(data_source,[DATA_PROCESSORS.remove_stopwords])
-print(processor.processed_data)  # Processed string
-processor = DataProcessor(data_source,[DATA_PROCESSORS.strip_tags])
-print(processor.processed_data)  # Processed string
