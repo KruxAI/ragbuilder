8d508702341c85728adfe2ee74581f1c2ae888f2
“ashwinaravind” <ashwin@krux.ai>
2024-12-11 14:29:45 +0530

    clean up- Generation

--- src/ragbuilder/langchain_module/chunkingstrategy/badlines.py
+++
@@ -1,5 +0,0 @@
-csv_file_path = "/Users/ashwinaravind/Desktop/kruxgitrepo/ragbuilder/ll_ap.csv"
-with open(csv_file_path, 'r') as f:
-    for line_number, line in enumerate(f, start=1):
-        if len(line.split(',')) != 29:  # Replace 29 with the expected number of fields
-            print(f"Issue in line {line_number}: {line}")
--- src/ragbuilder/langchain_module/chunkingstrategy/runtest1.py
+++
@@ -1,3 +0,0 @@
-from ragbuilder.langchain_module.chunkingstrategy.testprompt import run_test
-i=14
-run_test(str(i))
